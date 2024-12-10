1ff31502d77c0feae26eece36312bf98bcddd892
rlrq <rachelle2nd@yahoo.com.sg>
2024-12-10 15:53:07 +0800

    sync

--- encode_alignment_as_frequency.py
+++ encode_alignment_as_frequency.py
@@ -8,7 +8,7 @@ import basics
 from Bio import SeqIO
 
 ## replace all characters (except gaps) in alignment with rank of frequency in that position
-def encode_alignment_as_frequency(fin, fout, gap_char = '-', delim = ',', keep_description = False, index = 1):
+def encode_alignment_as_frequency(fin, fout, gap_char = '-', unknown_char = 'X', delim = ',', keep_description = False, index = 1):
     ## read sequences
     records = list(SeqIO.parse(fin, "fasta"))
     ## get longest sequence so we can iterate through all
@@ -17,15 +17,20 @@ def encode_alignment_as_frequency(fin, fout, gap_char = '-', delim = ',', keep_d
     ## create list to store encoded output as matrix first
     output = [[] for i in range(len(records))]
     ## iterate through each position
-    exclude_chars = ['-']
+    exclude_chars = [gap_char, '*', unknown_char]
     for i in range(max_length):
         ## get characters at position
         chars = [r.seq[i] for r in records]
+        # print(''.join(chars))
         ## encode as frequency rank
         chars_ranked = basics.order_to_dict(basics.order_elements_by_freq(chars, exclude = exclude_chars), index = index)
+        # print(chars_ranked)
         ## store output in alignment matrix
         for j, c in enumerate(chars):
-            output[j].append(chars_ranked.get(c, c))
+            encoded_char = chars_ranked.get(c, c)
+            # print(encoded_char, end = '')
+            output[j].append(encoded_char)
+        # print('')
     ## write encoded alignment
     make_record_id = (lambda record: record.description) if keep_description else (lambda record: record.id)
     with open(fout, "w+") as f:
@@ -40,10 +45,11 @@ parser.add_argument("fout", type=str, help="path to output delimited file")
 parser.add_argument("--keep-description", help="use full sequence description as ID in output", action="store_true", default=False)
 parser.add_argument("--delim", type=str, help="delimiter of output file", default=',')
 parser.add_argument("--gap-char", type=str, help="gap character", default='-')
+parser.add_argument("--unknown-char", type=str, help="unknown character; set to X for peptide, to N for nucleotide", default='X')
 parser.add_argument("--index", type=int, help="starting index for ranking", default=1)
 args = parser.parse_args()
 
 encode_alignment_as_frequency(args.fin, args.fout,
-                              gap_char = args.gap_char, delim = args.delim,
+                              gap_char = args.gap_char, unknown_char = args.unknown_char, delim = args.delim,
                               keep_description = args.keep_description, index = args.index)
 
