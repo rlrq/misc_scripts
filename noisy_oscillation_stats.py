ca08298847a215f6181624d498ac5b66d4a0875b
rlrq <rachelle2nd@yahoo.com.sg>
2025-04-02 01:08:53 +0800

    update

--- noisy_oscillation_stats.py
+++ noisy_oscillation_stats.py
@@ -2,22 +2,22 @@
 
 ## reads a series of values and extracts the following information:
 ## - 1/2 oscillation period (per 1/2 cycle, defined as the # of values that consecutively change in the same direction)
+## - 1/2 oscillation direction (per 1/2 cycle, direction of change of consecutive values; +/-)
 ## - 1/2 oscillation amplitude (per 1/2 cycle, defined as difference between the last & first value within each 1/2 cycle period)
 ## - 1/2 oscillation min (per 1/2 cycle, defined as the smaller value bounding a 1/2 cycle)
 ## - 1/2 oscillation max (per 1/2 cycle, defined as the larger value bounding a 1/2 cycle)
-## - 1/2 oscillation direction (per 1/2 cycle, direction of change of consecutive values; +/-)
 ## - 1/2 oscillation rate (per 1/2 cycle, 1/2 oscillation amplitude divided by 1/2 oscillation period)
 ## - smoothed 1/2 oscillation period (per 1/2 cycle, defined as 1/2 oscillation period, but allowing for changes in opposite direction less than a predefined magnitude)
-## - smoothed 1/2 oscillation amplitude (per 1/2 cycle, defined as the difference between the largest and smallest value within as smoothed 1/2 oscillation period)
+## - smoothed 1/2 oscillation direction (per 1/2 cycle, overall direction of change smoothed 1/2 oscillation period; +/-)
+## - smoothed 1/2 oscillation amplitude (per 1/2 cycle, defined as the difference between the largest and smallest value within a smoothed 1/2 oscillation period)
 ## - smoothed 1/2 oscillation min (per 1/2 cycle, smallest value in a smoothed 1/2 oscillation period)
 ## - smoothed 1/2 oscillation max (per 1/2 cycle, largest value in a smoothed 1/2 oscillation period)
-## - smoothed 1/2 oscillation direction (per 1/2 cycle, overall direction of change smoothed 1/2 oscillation period; +/-)
 ## - smoothed 1/2 oscillation rate (per 1/2 cycle, smoothed 1/2 oscillation amplitude divided by smoothed 1/2 oscillation period)
 ## - offset 1/2 oscillation period (per 1/2 cycle, defined as the # of values that are consecutively on the same side of the average value within a predefined window size that includes the first value of this period)
+## - offset 1/2 oscillation direction (per 1/2 cycle, whether the offset 1/2 oscillation period is above or below average; +/-)
 ## - offset 1 oscillation amplitude (per 1 cycle, defined as the difference between the largest and smallest value between 2 consecutive offset 1/2 oscillation periods)
 ## - offset 1/2 oscillation min (per 1/2 cycle, smallest value in a offset 1/2 oscillation period)
 ## - offset 1/2 oscillation max (per 1/2 cycle, largest value in a offset 1/2 oscillation period)
-## - offset 1/2 oscillation direction (per 1/2 cycle, whether the offset 1/2 oscillation period is above or below average; +/-)
 ## - offset 1 oscillation rate (per 1 cycle, offset 1 oscillation amplitude divided by 2 of the sum of the contributing offset 1/2 oscillation periods)
 
 
@@ -58,7 +58,7 @@ def neg_zero_pos(val):
 
 ## 1/2 oscillation period (per 1/2 cycle, defined as the # of values that consecutively change in the same direction)
 ## 1/2 oscillation direction (per 1/2 cycle, direction of change of consecutive values; +/-)
-def half_periods_direction(data):
+def half_period_direction(data):
     periods = [] ## tracks oscillation period length
     periods_dir = [] ## tracks oscillation period direction
     curr_period_len = 1
@@ -110,3 +110,92 @@ def half_max(data, periods):
 ## 1/2 oscillation rate (per 1/2 cycle, 1/2 oscillation amplitude divided by 1/2 oscillation period)
 def half_rate(periods, amplitudes):
     return [amplitudes[i]/periods[i] for i in range(len(periods))]
+
+
+########  SMOOTHED 1/2 OSCILLATION  ########
+
+## smoothed 1/2 oscillation period (per 1/2 cycle, defined as 1/2 oscillation period, but allowing for changes in opposite direction less than a predefined magnitude)
+## smoothed 1/2 oscillation direction (per 1/2 cycle, overall direction of change smoothed 1/2 oscillation period; +/-)
+def smoothed_half_period_direction(data, tolerance = 0):
+    periods = [] ## tracks oscillation period length
+    periods_dir = [] ## tracks oscillation period direction
+    curr_period_len = 1
+    last_diff = data[1] - data[0]
+    last_direction = neg_zero_pos(last_diff)
+    for i, v in enumerate(data[2:]):
+        i = i+2
+        curr_diff = data[i] - data[i-1]
+        curr_direction = neg_zero_pos(curr_diff)
+        if curr_direction != last_direction and (abs(curr_diff) > tolerance):
+            periods.append(curr_period_len)
+            periods_dir.append(last_direction)
+            curr_period_len = 1
+            last_direction = curr_direction
+        else:
+            curr_period_len += 1
+    periods.append(curr_period_len)
+    periods_dir.append(curr_direction)
+    return periods, periods_dir
+
+## smoothed 1/2 oscillation amplitude (per 1/2 cycle, defined as the difference between the largest and smallest value within a smoothed 1/2 oscillation period)
+def smoothed_half_period_amplitude(data, tolerance = 0):
+    output = []
+    i_start = 0
+    for period in periods:
+        i_end = i_start + period
+        values = data[i_start:i_end+1]
+        output.append(max(values) - min(values))
+        i_start = i_end
+    return output
+
+## smoothed 1/2 oscillation min (per 1/2 cycle, smallest value in a smoothed 1/2 oscillation period)
+def smoothed_half_min(data, periods):
+    output = []
+    i_start = 0
+    for period in periods:
+        i_end = i_start + period
+        values = data[i_start:i_end+1]
+        output.append(min(values))
+        i_start = i_end
+    return output
+
+## smoothed 1/2 oscillation max (per 1/2 cycle, largest value in a smoothed 1/2 oscillation period)
+def smoothed_half_max(data, periods):
+    output = []
+    i_start = 0
+    for period in periods:
+        i_end = i_start + period
+        values = data[i_start:i_end+1]
+        output.append(max(values))
+        i_start = i_end
+    return output
+
+## smoothed 1/2 oscillation rate (per 1/2 cycle, smoothed 1/2 oscillation amplitude divided by smoothed 1/2 oscillation period)
+def half_rate(periods, amplitudes):
+    return [amplitudes[i]/periods[i] for i in range(len(periods))]
+
+
+########  OFFSET 1/2 OSCILLATION  ########
+
+## offset 1/2 oscillation period (per 1/2 cycle, defined as the # of values that are consecutively on the same side of the average value within a predefined window size that includes the first value of this period)
+## offset 1/2 oscillation direction (per 1/2 cycle, whether the offset 1/2 oscillation period is above or below average; +/-)
+def offset_half_period_direction(data, window = 10, overlap = 0):
+    periods = [] ## tracks oscillation period length
+    periods_dir = [] ## tracks oscillation period direction
+    curr_period_len = 1
+    last_diff = data[1] - data[0]
+    last_direction = neg_zero_pos(last_diff)
+    for i, v in enumerate(data[2:]):
+        i = i+2
+        curr_diff = data[i] - data[i-1]
+        curr_direction = neg_zero_pos(curr_diff)
+        if curr_direction != last_direction and (abs(curr_diff) > tolerance):
+            periods.append(curr_period_len)
+            periods_dir.append(last_direction)
+            curr_period_len = 1
+            last_direction = curr_direction
+        else:
+            curr_period_len += 1
+    periods.append(curr_period_len)
+    periods_dir.append(curr_direction)
+    return periods, periods_dir
