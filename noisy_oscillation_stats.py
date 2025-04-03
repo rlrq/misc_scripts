3972bc21aee52a4c09d0d119004411111f33d65e
rlrq <rachelle2nd@yahoo.com.sg>
2025-04-03 15:56:37 +0800

    update

--- noisy_oscillation_stats.py
+++ noisy_oscillation_stats.py
@@ -20,6 +20,9 @@
 ## - offset 1/2 oscillation max (per 1/2 cycle, largest value in a offset 1/2 oscillation period)
 ## - offset 1 oscillation rate (per 1 cycle, offset 1 oscillation amplitude divided by 2 of the sum of the contributing offset 1/2 oscillation periods)
 
+## misc:
+## - non-overlapping period start: takes periods, and outputs list of period starting indices (offset by 0 by default, modifiable by user)
+
 
 #################
 ##  TEST DATA  ##
@@ -48,6 +51,12 @@ var_dat = subset_column("P2", header_inpt, raw_inpt)
 
 def neg_zero_pos(val):
     return 0 if val == 0 else (1 if val > 0 else -1)
+
+def period_starts_nonoverlapping(periods, offset = 0):
+    output = [offset]
+    for v in periods[:-1]:
+        output.append(output[-1] + v)
+    return tuple(output)
 
 
 #######################
@@ -112,14 +121,15 @@ def half_rate(periods, amplitudes):
     return tuple(amplitudes[i]/periods[i] for i in range(len(periods)))
 
 ## get all 1/2 oscillation stats
-def half_oscillation_stats(data):
+def half_oscillation_stats(data, offset = 0):
     periods, directions = half_period_direction(data)
+    starts = period_starts_nonoverlapping(periods, offset = offset)
     amplitudes = half_amplitude(data, periods)
     min_vals = half_min(data, periods)
     max_vals = half_max(data, periods)
     rates = half_rate(periods, amplitudes)
-    return {"period": periods, "direction": directions, "amplitude": amplitudes,
-            "min": min_vals, "max": max_vals, "rate": rates}
+    return {"start": starts, "period": periods, "direction": directions,
+            "amplitude": amplitudes, "min": min_vals, "max": max_vals, "rate": rates}
 
 ########  SMOOTHED 1/2 OSCILLATION  ########
 
@@ -184,14 +194,15 @@ def smoothed_half_rate(periods, amplitudes):
     return tuple(amplitudes[i]/periods[i] for i in range(len(periods)))
 
 ## get all smoothed 1/2 oscillation stats
-def smoothed_half_oscillation_stats(data, tolerance = 0):
+def smoothed_half_oscillation_stats(data, tolerance = 0, offset = 0):
     periods, directions = smoothed_half_period_direction(data, tolerance = tolerance)
+    starts = period_starts_nonoverlapping(periods, offset = offset)
     amplitudes = smoothed_half_amplitude(data, periods)
     min_vals = smoothed_half_min(data, periods)
     max_vals = smoothed_half_max(data, periods)
     rates = smoothed_half_rate(periods, amplitudes)
-    return {"period": periods, "direction": directions, "amplitude": amplitudes,
-            "min": min_vals, "max": max_vals, "rate": rates}
+    return {"start": starts, "period": periods, "direction": directions,
+            "amplitude": amplitudes, "min": min_vals, "max": max_vals, "rate": rates}
 
 
 ########  OFFSET 1/2 OSCILLATION  ########
